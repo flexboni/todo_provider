5d99a57e8c2634d87d9f7f6b2ee0a80c8ac420cf
boni <kgb8594@gmail.com>
2022-08-14 21:55:07 +0900

    Chapter 49. ShowTodos widget

--- lib/pages/todos_page.dart
+++ lib/pages/todos_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_provider/model/todo_model.dart';
-import 'package:todo_provider/provider/active_todo_count.dart';
 import 'package:todo_provider/provider/providers.dart';
 
 class TodosPage extends StatefulWidget {
@@ -28,6 +27,7 @@ class _TodosPageState extends State<TodosPage> {
                 CreateTodo(),
                 const SizedBox(height: 20),
                 SearchAndFilterTodo(),
+                ShowTodos(),
               ],
             ),
           ),
@@ -142,5 +142,46 @@ class SearchAndFilterTodo extends StatelessWidget {
   Color textColor(BuildContext context, Filter filter) {
     final currentFilter = context.watch<TodoFilter>().state.filter;
     return currentFilter == filter ? Colors.blue : Colors.grey;
+  }
+}
+
+class ShowTodos extends StatelessWidget {
+  const ShowTodos({Key? key}) : super(key: key);
+
+  Widget showBackground(int direction) {
+    return Container(
+      margin: const EdgeInsets.all(4),
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      color: Colors.red,
+      alignment: direction == 0 ? Alignment.centerLeft : Alignment.centerRight,
+      child: Icon(
+        Icons.delete,
+        size: 30,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final todos = context.watch<FilteredTodos>().state.filteredTodos;
+
+    return ListView.separated(
+      primary: false,
+      shrinkWrap: true,
+      itemCount: todos.length,
+      separatorBuilder: (context, index) => Divider(color: Colors.grey),
+      itemBuilder: (context, index) {
+        return Dismissible(
+          key: ValueKey(todos[index].id),
+          background: showBackground(0),
+          secondaryBackground: showBackground(1),
+          child: Text(
+            todos[index].desc,
+            style: TextStyle(fontSize: 20),
+          ),
+        );
+      },
+    );
   }
 }
